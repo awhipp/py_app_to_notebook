2ea3d88148ab43391ffd5ce1da5117b1259e1ab4
Alexander Whipp <alexruns4@gmail.com>
2024-02-01 19:37:26 -0500

    feat: :construction: Working recursive dependency tree

--- py_app_to_notebook/utilities/dir.py
+++ py_app_to_notebook/utilities/dir.py
@@ -4,93 +4,86 @@ Directory utilities, specifically used to determine the dependencies of a Python
 import ast
 import os
 
-def is_internal_dependency(module_name, root_folder):
+from typing import Optional
+
+def module_name_to_path(module_name) -> str:
     """
-    Check if a module is an internal dependency (part of the source codebase).
+    Convert a module name to a path.
 
     Args:
     - module_name (str): The name of the module.
-    - root_folder (str): The root folder containing Python files.
 
     Returns:
-    - bool: True if the module is internal, False otherwise.
+    - str: The path to the module.
     """
-    module_path = module_name.replace('.', os.sep)
-    return os.path.exists(module_path + '.py') or os.path.exists(module_path)
+    return module_name.replace('.', os.sep) + '.py'
 
-def get_dependencies(file_path: str, root_folder: str):
-    """
-    Get the dependency tree of a Python file based on its imports.
+class Dependency():
+    """Represents a modules, its name and path"""
 
-    Args:
-    - file_path (str): The path to the Python file.
-    - root_folder (str): The root folder containing Python files.
+    path: str
+    module_name: str
+    name: str
+    dependencies: set
 
-    Returns:
-    - set: A set containing the names of the modules the file depends on.
-    """
-    dependencies = set()
+    def __init__(self, root: str, path: str, parent: str = ""):
+        """From a file path, create a dependency object."""
+        self.root = root
+        self.path = path.replace('\\', os.sep).replace('/', os.sep)
+        if parent == "":
+            self.module_name = os.path.basename(path).replace('.py', '')
+        else:
+            self.module_name = parent + "." + os.path.basename(path).replace('.py', '')
+        self.name = os.path.basename(path).replace('.py', '')
+        self.dependencies = set()
+        
+        self.find_dependencies()
 
-    with open(file_path, 'r', encoding='utf-8') as file:
-        tree = ast.parse(file.read(), filename=file_path)
+    def __str__(self):
+        """String representation of the dependency."""
+        return f"{self.module_name} ({self.path})"
+    
+    def print_dependency_tree(self, level: int = 1):
+        """Print the dependency tree."""
+        print(self)
+        for dependency in self.dependencies:
+            print("----" * level, end="")
+            dependency.print_dependency_tree(level + 1)
 
-    for node in ast.walk(tree):
-        if isinstance(node, ast.Import):
-            for alias in node.names:
-                if is_internal_dependency(alias.name, root_folder):
-                    dependencies.add(alias.name)
-        elif isinstance(node, ast.ImportFrom):
-            module_name = node.module
-            if module_name is not None and is_internal_dependency(module_name, root_folder):
-                dependencies.add(module_name)
+    def find_dependencies(self):
+        """
+        Get the dependency tree of a Python file based on its imports.
+        """
 
-    return dependencies
+        with open(self.path, 'r', encoding='utf-8') as file:
+            tree = ast.parse(file.read(), filename=self.path)
 
-def print_dependency_tree(dependency_tree: dict[str, str], node: str, indentation=""):
-    result = f"{indentation}- {os.path.basename(node)}"
-    print(dependency_tree)
-    print("---- " + node)
-    for dependency in dependency_tree[node]:
-        result += "\n" + print_dependency_tree(dependency, indentation + "  ")
-    return result
+        for node in ast.walk(tree):
+            if isinstance(node, ast.Import):
+                for alias in node.names:
+                    module_path = module_name_to_path(alias.name)
+                    if self.root in module_path:
+                        self.dependencies.add(
+                            Dependency(
+                                root=self.root,
+                                path=module_path,
+                                parent=self.module_name
+                            )
+                        )
+            elif isinstance(node, ast.ImportFrom):
+                module_name = node.module
+                if module_name is not None:
+                    module_path = module_name_to_path(module_name)
+                    if self.root in module_path:
+                        self.dependencies.add(
+                            Dependency(
+                                root=self.root,
+                                path=module_path,
+                                parent=self.module_name)
+                        )
 
-def analyze_dependency_tree(root_folder: str, skip_tests: bool = True):
-    """
-    Analyze the dependency tree of all Python files in a given folder.
-
-    Args:
-    - root_folder (str): The root folder containing Python files.
-    - skip_tests (bool): Whether to skip test files.
-
-    Returns:
-    - dict: A dictionary where keys are Python file names, and values are sets
-            containing the names of the modules each file depends on.
-    """
-    dependency_tree = {}
-
-    for root, dirs, files in os.walk(root_folder):
-        for file_name in files:
-            if skip_tests and 'test' in file_name.lower():
-                continue
-            if "site-packages" in root or ".venv" in root or "__init__" in file_name:
-                continue
-            if file_name.endswith('.py'):
-                file_path = os.path.join(root, file_name)
-                dependencies = get_dependencies(file_path, root_folder)
-                dependency_tree[file_path] = dependencies
-
-    return dependency_tree
-
+        
 if __name__ == '__main__':
-    # Example usage:
-    root_folder_path = 'queue_to_s3_sample/'
-    dependency_tree = analyze_dependency_tree(root_folder_path)
-
-    # Print 
-    tree_representation = ""
-    for node in dependency_tree:
-        tree_representation += print_dependency_tree(dependency_tree, node) + "\n"
-
-    print(
-        tree_representation.strip()
-    )
+    entry_point = 'queue_to_s3_sample/app.py'
+    dep = Dependency(root='queue_to_s3_sample', path=entry_point)
+    dep.print_dependency_tree()
