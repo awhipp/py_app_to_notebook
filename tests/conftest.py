612d1e669ccd4e2002b68edd23271e10aa548065
awhipp <whipp_alexander@bah.com>
2024-02-06 00:36:16 -0500

    test: :white_check_mark: Fixing indent for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,12 +6,11 @@ def output_dependency_tree():
     """Output of the dependency tree."""
     
     expected_tree = """app (queue_to_s3_sample/app.py)
-----app.sqs (queue_to_s3_sample/aws_helpers/sqs.py)
---------app.sqs.boto_helpers (queue_to_s3_sample/aws_helpers/boto_helpers.py)
-----app.s3 (queue_to_s3_sample/aws_helpers/s3.py)
---------app.s3.boto_helpers (queue_to_s3_sample/aws_helpers/boto_helpers.py)
-----app.boto_helpers (queue_to_s3_sample/aws_helpers/boto_helpers.py)
-"""
+----app.sqs_utils (queue_to_s3_sample/utilities/sqs/sqs_utils.py)
+--------app.sqs_utils.boto_helpers (queue_to_s3_sample/utilities/boto_helpers.py)
+----app.s3_utils (queue_to_s3_sample/utilities/s3/s3_utils.py)
+--------app.s3_utils.boto_helpers (queue_to_s3_sample/utilities/boto_helpers.py)
+----app.boto_helpers (queue_to_s3_sample/utilities/boto_helpers.py)\n"""
     expected_tree = expected_tree.replace('/', os.sep)
 
     return expected_tree
@@ -22,9 +21,9 @@ def output_dependency_tree_list():
     """Output of the dependency tree as a list."""
     expected_list = [
         'queue_to_s3_sample/app.py',
-        'queue_to_s3_sample/aws_helpers/sqs.py',
-        'queue_to_s3_sample/aws_helpers/boto_helpers.py',
-        'queue_to_s3_sample/aws_helpers/s3.py',
+        'queue_to_s3_sample/utilities/sqs/sqs_utils.py',
+        'queue_to_s3_sample/utilities/boto_helpers.py',
+        'queue_to_s3_sample/utilities/s3/s3_utils.py',
     ]
 
     for idx, item in enumerate(expected_list):
